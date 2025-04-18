c93bc1951cf19a0a0d7d5ec0665056fc25e050c4
Joy Hancock <86928974+joy-hancock@users.noreply.github.com>
2025-04-18 17:47:35 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('it works');
+window.addEventListener('load', function () {
+	baguetteBox.run('.gallery');
+});
+
+const gallerySearch = document.getElementById('gallerySearch');
+
+gallerySearch.addEventListener('keyup', (e) => {
+	let currentValue = e.target.value.toLowerCase();
+
+	let images = document.querySelectorAll('a.gallery-img');
+	images.forEach((image) => {
+		let caption = image.getAttribute('data-caption');
+		if (caption.toLowerCase().includes(currentValue)) {
+			image.style.display = 'block';
+		} else {
+			image.style.display = 'none';
+		}
+	});
+});
